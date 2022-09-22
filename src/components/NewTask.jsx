37496e1508aca80be6ab076cb6b14a2f40fe2600
tyfnacici <tayfunacici676767@gmail.com>
2022-09-23 00:36:27 +0300

    Bugs fixed

--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,70 +1,37 @@
-import { React, useEffect, useCallback, useState, useRef } from "react";
-import { db } from "../firebase";
+import { React, useState } from "react";
 
-//Veritabanından veri çekiliyor zaten fakat verilerin ekrana çıktısı verilemiyor gelen objeler dizide olmadığı için. Bunu arraya çevirip çıktı alabilirsin. Ek olarak:Firebasede firestorm veya realtime database kullanarak verileri silebilmeyi, düzenleyebilmeyi bir de taskın yanına tık atmak için kutu koy. bunu işaretleyince taskın üzeri çizilmiş olsun, veritabanında 0=>1 olsun en aşağıda düşsün.
+let i = 0;
+
 const NewTask = (props) => {
-  const headerRef = useRef("");
-  const descRef = useRef("");
+  const [header, setHeader] = useState("");
+  const [desc, setDesc] = useState("");
 
-  const submitHandler = async (event) => {
+  const newTaskHandler = (event) => {
     event.preventDefault();
-
-    const task = {
-      header: headerRef.current.value,
-      desc: descRef.current.value,
+    i++;
+    const newTask = {
+      id: i.toString(),
+      header: header,
+      desc: desc,
     };
-    // fetch(
-    //   "https://todo-list-37c01-default-rtdb.europe-west1.firebasedatabase.app/tasks.json",
-    //   {
-    //     method: "POST",
-    //     body: JSON.stringify(task),
-    //     headers: { "Content-Type": "application/json" },
-    //   }
-    // );
-    const uuid = uid();
-    set(ref(db, `${uuid}`), task);
+    props.taskHandler(newTask);
+    setHeader("");
+    setDesc("");
   };
 
-  const taskFetchHandler = useCallback(async (event) => {
-    event.preventDefault();
-    const response = fetch(
-      "https://todo-list-37c01-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-    )
-      .then(props.setIsLoading(1))
-      .then((response) => response.json())
-      .then(
-        setTimeout(() => {
-          props.setIsLoading(2);
-        }, 1000)
-      )
-      .then((data) => props.setTasks(data.results))
-      .catch(alert("An unexpected thing happen :("));
-  });
-
-  useEffect(() => {
-    const response = fetch(
-      "https://todo-list-37c01-default-rtdb.europe-west1.firebasedatabase.app/tasks.json"
-    )
-      .then(props.setIsLoading(1))
-      .then((response) => response.json())
-      .then(
-        setTimeout(() => {
-          props.setIsLoading(2);
-        }, 1000)
-      )
-      .then((data) => console.log(data))
-      .then((data) => props.setTasks(data));
-  }, []);
-
   return (
-    <form className="flex flex-col place-content-between space-y-3 p-6 bg-slate-800 text-white rounded-xl w-4/5 lg:w-4/5">
+    <form
+      onSubmit={newTaskHandler}
+      className="flex flex-col place-content-between space-y-3 p-6 bg-slate-800 text-white rounded-xl w-4/5 lg:w-4/5"
+    >
       <div className="flex flex-col">
         <label>Task Name</label>
         <input
           placeholder="Enter a task"
           className="border-2 border-orange-500 pl-3 py-3 rounded-xl text-black"
           type="text"
-          ref={headerRef}
+          onChange={(e) => setHeader(e.target.value)}
+          value={header}
         />
       </div>
       <div className="flex flex-col">
@@ -73,22 +40,16 @@ const NewTask = (props) => {
           placeholder="Enter a task description"
           className="border-2 border-orange-500 pl-3 py-3 rounded-xl text-black"
           type="text"
-          ref={descRef}
+          onChange={(e) => setDesc(e.target.value)}
+          value={desc}
         />
       </div>
       <div className="flex space-x-4">
         <button
           type="submit"
-          onClick={submitHandler}
           className="px-2 grow py-3.5 self-end bg-orange-500 rounded-xl hover:bg-white hover:text-orange-500"
         >
           Add task
-        </button>
-        <button
-          onClick={taskFetchHandler}
-          className="px-2 grow py-3.5 self-end bg-orange-500 rounded-xl hover:bg-white hover:text-orange-500"
-        >
-          Refresh
         </button>
       </div>
     </form>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,7 +8,7 @@ const Tasks = (props) => {
     <motion.div
       layout
       onClick={() => setIsOpen(!isOpen)}
-      transition={{ layout: { duration: 0.1 } }}
+      transition={{ layout: { duration: 0.15 } }}
       className="text-white bg-slate-800 flex-col lg:flex-row mt-3 p-4 flex place-content-between space-x-3 rounded-xl w-4/5 lg:w-4/5"
     >
       <motion.div
@@ -16,10 +16,10 @@ const Tasks = (props) => {
         className="lg:text-left text-center py-6 lg:py-0"
       >
         <h1 className="text-3xl text-orange-500">{props.header}</h1>
-        {!isOpen && props.isLoading !== 1 && <p>Click to expand</p>}
+        {!isOpen && <p>Click to expand</p>}
         {isOpen && <p>{props.desc}</p>}
       </motion.div>
-      {props.isLoading !== 1 && (
+      {/* {props.isLoading !== 1 && (
         <motion.div layout="position" className="self-center flex space-x-3">
           <button className="lg:px-2 px-4 py-4 lg:py-2 rounded-xl hover:bg-white border-2 hover:text-green-500 border-green-500 bg-green-500 text-white">
             Edit Task
@@ -28,7 +28,7 @@ const Tasks = (props) => {
             Delete
           </button>
         </motion.div>
-      )}
+      )} */}
     </motion.div>
   );
 };
